25e369b1e1ce030004dc639e3a76e76e1ca967f6
Josh Goebel <me@joshgoebel.com>
2020-08-11 08:54:20 -0400

    nim wrapper

--- src/exercism_local_tooling_webserver.nim
+++ src/exercism_local_tooling_webserver.nim
@@ -21,26 +21,14 @@ routes:
     let zip_file = fmt"{job_dir}/files.gzip"
     let zip_data = request.formData.getOrDefault("zipped_files").body
     writeFile(zip_file, zip_data)
-    # File.write(zip_file, params[:zipped_files])
     discard execShellCmd fmt"unzip {zip_file} -d {input_dir}"
-    # logger.info `ls -lah #{input_dir}`
     echo execProcess(fmt"ls -lah {input_dir}")
 
     # Run command
-    # let exercise_name = params[:exercise]
     let exercise_name = request.formData.getOrDefault("exercise").body
     let cmd = fmt"./bin/run.sh {exercise_name} {input_dir} {output_dir}"
-    # setCurrentDir("/opt/test-runner")
+    setCurrentDir("/opt/test-runner")
     let exit_status = execShellCmd(cmd)
-    # exit_status = Dir.chdir("/opt/test-runner/") do
-    #     system(cmd)
-    # end
-
-    # # Read and return the result
-    # begin
-    #     result = File.read("#{output_dir}/#{params[:results_filepath]}")
-    # rescue Errno::ENOENT
-    # end
 
     let response = %*
         {
@@ -53,7 +41,3 @@ routes:
         response["result"] = % readFile(fmt"{output_dir}/{results_filepath}")
 
     resp response
-    # json(
-    #     exit_status: exit_status,
-    #     result: result
-    # )
